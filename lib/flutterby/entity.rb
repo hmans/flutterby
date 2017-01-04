74c3a71081d488c8079667af678f70eac072bb8c
Hendrik Mans <hendrik@mans.de>
2017-01-04 11:57:56 +0100

    Only files should be processed

--- lib/flutterby/entity.rb
+++ lib/flutterby/entity.rb
@@ -30,16 +30,6 @@ module Flutterby
     end
 
     def process
-      while ext = extensions.pop do
-        klass = case ext
-        when "md" then SlodownProcessor
-        end
-
-        if klass
-          processor = klass.new(@contents)
-          @contents = processor.process
-        end
-      end
     end
 
     def write(path)
--- lib/flutterby/file.rb
+++ lib/flutterby/file.rb
@@ -4,6 +4,19 @@ module Flutterby
       @contents = ::File.read(@path)
     end
 
+    def process
+      while ext = extensions.pop do
+        klass = case ext
+        when "md" then SlodownProcessor
+        end
+
+        if klass
+          processor = klass.new(@contents)
+          @contents = processor.process
+        end
+      end
+    end
+
     def write(path)
       ::File.write(path, @contents)
     end
