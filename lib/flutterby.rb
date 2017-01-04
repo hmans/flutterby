8d55158ba3b1053c17cbdff9f13a4dcd8197a56e
Hendrik Mans <hendrik@mans.de>
2017-01-04 11:46:53 +0100

    Rename Processor to Runner

--- lib/flutterby.rb
+++ lib/flutterby.rb
@@ -1,4 +1,5 @@
+require_relative "flutterby/processor"
 require_relative "flutterby/entity"
 require_relative "flutterby/file"
 require_relative "flutterby/folder"
-require_relative "flutterby/processor"
+require_relative "flutterby/runner"
