742f25761ee8eb10cbd8d8d53f0295d7db02379b
Hendrik Mans <hendrik@mans.de>
2017-01-05 11:31:49 +0100

    Add some CLI code

--- lib/flutterby/cli.rb
+++ lib/flutterby/cli.rb
@@ -4,4 +4,18 @@ Commander.configure do
   program :name, 'Flutterby'
   program :version, Flutterby::VERSION
   program :description, 'There are many static site generators. This one is mine.'
+
+  command :build do |c|
+    c.syntax = 'flutterby build [options]'
+    c.description = "Build your website."
+
+    c.option '--in DIR', String, "Directory containing your source files"
+    c.option '--out DIR', String, "Target directory"
+
+    c.action do |args, options|
+      options.default in: "./site/", out: "./_build/"
+      say "Yo."
+      puts options.inspect
+    end
+  end
 end
