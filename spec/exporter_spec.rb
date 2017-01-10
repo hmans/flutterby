ef2875890d979a690ac473e8e034aee4b308297a
Hendrik Mans <hendrik@mans.de>
2017-01-10 12:45:35 +0100

    More exporter specs

--- spec/exporter_spec.rb
+++ spec/exporter_spec.rb
@@ -1,19 +1,7 @@
 require 'flutterby/exporter'
 
 describe Flutterby::Exporter do
-  def export_path
-    File::expand_path("../../tmp/flutterby_export/", __FILE__)
-  end
-
-  def cleanup!
-    FileUtils.rm_rf(export_path)
-  end
-
-  def build!
-    root = read "/"
-    Flutterby::Exporter.new(root)
-      .export!(into: export_path)
-  end
+  include ExportHelpers
 
   before(:all) do
     cleanup!
@@ -24,6 +12,17 @@ describe Flutterby::Exporter do
     cleanup!
   end
 
-  specify "it should build a site" do
+  specify "renders markdown to HTML" do
+    expect(generated_file("markdown.html"))
+      .to eq(%{\n<h1 id="this-is-markdown">This is Markdown</h1>\n\n<p>It’s great!</p>\n})
+  end
+
+  specify "renders Scss to CSS" do
+    expect(generated_file("css/styles.css"))
+      .to eq(%{body {\n  color: red; }\n})
+  end
+
+  specify "creates subdirectories" do
+    expect(File.directory?(generated_path("/posts"))).to be_truthy
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "flutterby"
+require "pry"
 
 Flutterby.logger.level = Logger::FATAL
 
@@ -9,7 +10,31 @@ module Helpers
   end
 
   def read(name)
-    Flutterby.from ::File.join(site_path, name)
+    Flutterby.from ::File.join(site_path, name), name: name
+  end
+end
+
+module ExportHelpers
+  def export_path
+    File::expand_path("../../tmp/flutterby_export/", __FILE__)
+  end
+
+  def cleanup!
+    FileUtils.rm_rf(export_path)
+  end
+
+  def build!
+    root = read "/"
+    Flutterby::Exporter.new(root)
+      .export!(into: export_path)
+  end
+
+  def generated_path(path)
+    File.join(export_path, path)
+  end
+
+  def generated_file(path)
+    File.read(generated_path(path))
   end
 end
 
