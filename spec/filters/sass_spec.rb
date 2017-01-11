4936be7b49e52b8fbcd239aa7b5d246ba46cba7c
Hendrik Mans <hendrik@mans.de>
2017-01-11 16:13:51 +0100

    Fix specs for 2.3

--- spec/filters/sass_spec.rb
+++ spec/filters/sass_spec.rb
@@ -1,7 +1,9 @@
 describe "Sass Filter" do
   subject { read "css/styles.css.scss" }
 
-  let :expected { "strong {\n  color: green; }\n\nbody {\n  color: red; }\n" }
+  let :expected do
+    "strong {\n  color: green; }\n\nbody {\n  color: red; }\n"
+  end
 
   specify "converted into CSS, with working partials" do
     expect(subject.body).to eq(expected)
