8953800e22a39098da4f733769b0cf71e8765bdf
Hendrik Mans <hendrik@mans.de>
2017-01-13 21:24:10 +0100

    Clean up node extension spec a little

--- spec/node_extension_spec.rb
+++ spec/node_extension_spec.rb
@@ -1,16 +1,16 @@
 describe "extending all nodes in a folder through _node.rb" do
   let!(:root) do
-    Flutterby::Node.new "/"
+    node "/"
   end
 
   let!(:page) do
-    Flutterby::Node.new "page.html.erb", parent: root, source: <<~EOF
+    node "page.html.erb", parent: root, source: <<~EOF
     I'm a page! This is a <%= node.show_test %>!
     EOF
   end
 
   let!(:extension) do
-    Flutterby::Node.new "_node.rb", parent: root, source: <<~EOF
+    node "_node.rb", parent: root, source: <<~EOF
     def show_test
       "test"
     end
