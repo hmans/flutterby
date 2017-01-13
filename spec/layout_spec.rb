7f61bca4a9956a5fee91919bc7bee8da1cedc465
Hendrik Mans <hendrik@mans.de>
2017-01-13 16:54:53 +0100

    Implement layout spec

--- spec/layout_spec.rb
+++ spec/layout_spec.rb
@@ -1,3 +1,35 @@
 describe "layouts" do
-  pending
+  let!(:root) { node "/" }
+
+  let!(:outer_layout) do
+    node "_layout.erb", parent: root, source: <<~EOF
+    <h1>Outer Layout</h1>
+    <%= yield %>
+    EOF
+  end
+
+  let!(:folder) do
+    node "test", parent: root
+  end
+
+  let!(:page) do
+    node "page.html", parent: folder, source: <<~EOF
+    <p>I'm the actual page!</p>
+    EOF
+  end
+
+  let!(:inner_layout) do
+    node "_layout.erb", parent: folder, source: <<~EOF
+    <h2>Inner Layout</h2>
+    <%= yield %>
+    EOF
+  end
+
+  let(:expected_output) do
+    %{<h1>Outer Layout</h1>\n<h2>Inner Layout</h2>\n<p>I'm the actual page!</p>\n\n\n}
+  end
+
+  specify do
+    expect(page.render(layout: true)).to eq(expected_output)
+  end
 end
