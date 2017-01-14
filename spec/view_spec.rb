201d9369b9c6abaa5d55c2f47301d201220a8dd1
Hendrik Mans <hendrik@mans.de>
2017-01-14 22:24:42 +0100

    Add html_escape et al

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -1,5 +1,5 @@
 describe Flutterby::View do
-  subject { node "page.html.erb", source: %{<%= raw "<g>" %>} }
+  subject { node "page.html.erb", source: source }
 
   describe '#raw' do
     let(:source) { %{<%= raw "<g>" %>} }
@@ -7,6 +7,12 @@ describe Flutterby::View do
   end
 
   describe '#html_escape' do
-    pending  # NOTE: test View instance instead
+    let(:source) { %{<%= raw(html_escape "<g>") %>} }
+    its(:body) { is_expected.to eq('&lt;g&gt;') }
+  end
+
+  describe '#h' do
+    let(:source) { %{<%= raw(h "<g>") %>} }
+    its(:body) { is_expected.to eq('&lt;g&gt;') }
   end
 end
