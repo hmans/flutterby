e68136521e5265494cf81e0cf42f2d240f8826c1
Hendrik Mans <hendrik@mans.de>
2017-01-22 15:20:04 +0100

    Improve Ruby node spec

--- spec/filters/ruby_node_spec.rb
+++ spec/filters/ruby_node_spec.rb
@@ -1,5 +1,5 @@
 describe "Ruby nodes" do
-  def ruby_code
+  let :ruby_code do
     <<~EOF
     tag(:p) { "I'm the body!" }
     EOF
@@ -13,5 +13,20 @@ describe "Ruby nodes" do
     expect(subject).to be_kind_of(Flutterby::Node)
     expect(subject.ext).to eq("html")
     expect(subject.render).to eq("<p>I&#39;m the body!</p>")
+  end
+
+  context "with instance variables" do
+    let :ruby_code do
+      <<~EOF
+      @message = "Hooray, instance variables in views!"
+      tag(:p) { @message }
+      EOF
+    end
+
+    its(:render) { is_expected.to eq("<p>Hooray, instance variables in views!</p>") }
+
+    it "does not set the instance variable on the node" do
+      expect(subject.instance_variable_get("@message")).to be_nil
+    end
   end
 end
