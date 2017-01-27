7f83f92ac3c2cddebe3f9738608c013df9dca099
Hendrik Mans <hendrik@mans.de>
2017-01-27 13:44:02 +0100

    Clean up node spec to use new #create syntax

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,25 +1,26 @@
 describe Flutterby::Node do
-  let!(:folder_node) { Flutterby::Node.new("a_folder") }
-  let!(:file_node)   { Flutterby::Node.new("file.txt", parent: folder_node) }
-  let!(:page_node)   { Flutterby::Node.new("file.html.md", parent: folder_node) }
+  let!(:root)   { node "/" }
+  let!(:folder) { root.create("a_folder") }
+  let!(:file)   { folder.create("file.txt") }
+  let!(:page)   { folder.create("file.html.md") }
 
   describe '#page?, #file? and #folder?' do
     context "HTML page nodes" do
-      subject { page_node }
+      subject { page }
       it { is_expected.to be_page }
       it { is_expected.to be_file }
       it { is_expected.to_not be_folder }
     end
 
     context "file nodes" do
-      subject { file_node }
+      subject { file }
       it { is_expected.to_not be_page }
       it { is_expected.to be_file }
       it { is_expected.to_not be_folder }
     end
 
     context "folder nodes" do
-      subject { folder_node }
+      subject { folder }
       it { is_expected.to_not be_page }
       it { is_expected.to_not be_file }
       it { is_expected.to be_folder }
@@ -27,26 +28,26 @@ describe Flutterby::Node do
   end
 
   describe '#create' do
-    subject { folder_node.create("test.html") }
+    subject { folder.create("test.html") }
 
-    its(:parent) { is_expected.to eq(folder_node) }
+    its(:parent) { is_expected.to eq(folder) }
     its(:name) { is_expected.to eq('test') }
     its(:ext) { is_expected.to eq('html') }
 
     context 'when another parent is specified' do
-      subject { folder_node.create("test.html", parent: file_node) }
-      its(:parent) { is_expected.to eq(folder_node) }
+      subject { folder.create("test.html", parent: file) }
+      its(:parent) { is_expected.to eq(folder) }
     end
   end
 
   describe '#siblings' do
     context "when there's a parent" do
-      subject { page_node }
-      its(:siblings) { is_expected.to eq([file_node]) }
+      subject { page }
+      its(:siblings) { is_expected.to eq([file]) }
     end
 
     context "when there's no parent" do
-      subject { folder_node }
+      subject { root }
       its(:siblings) { is_expected.to eq(nil) }
     end
   end
