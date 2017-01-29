6e20ff3c95a003c32fbb82b449b5a94167a3cdb8
Hendrik Mans <hendrik@mans.de>
2017-01-29 19:35:00 +0100

    Implement Deletion spec

--- spec/node/deletion_spec.rb
+++ spec/node/deletion_spec.rb
@@ -1,3 +1,16 @@
 describe Flutterby::Deletion do
-  pending
+  subject { node "page.html" }
+
+  describe '#delete!' do
+    it "emits a :deleted event" do
+      expect(subject).to receive(:emit).with(:deleted)
+      subject.delete!
+    end
+
+    it "marks the node as deleted" do
+      expect { subject.delete! }
+        .to change { subject.deleted? }
+        .from(false).to(true)
+    end
+  end
 end
