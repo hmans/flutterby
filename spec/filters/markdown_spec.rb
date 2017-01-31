891d5757c8f8a04232253f36ad7985104da4e55a
Hendrik Mans <hendrik@mans.de>
2017-01-31 10:43:43 +0100

    Minor spec cleanup

--- spec/filters/markdown_spec.rb
+++ spec/filters/markdown_spec.rb
@@ -13,9 +13,8 @@ puts "OMG"
 EOF
   end
 
-  let :expected_body do
-    %{<h1 id=\"this-is-markdown\">This is Markdown</h1>\n\n<p>It’s great! Here’s some Ruby code:</p>\n\n<pre><code class=\"language-ruby\">puts \"OMG\"\n</code></pre>\n}
+  it "renders the markdown to HTML" do
+    expect(subject.render)
+      .to eq %{<h1 id=\"this-is-markdown\">This is Markdown</h1>\n\n<p>It’s great! Here’s some Ruby code:</p>\n\n<pre><code class=\"language-ruby\">puts \"OMG\"\n</code></pre>\n}
   end
-
-  its(:render) { is_expected.to eq(expected_body) }
 end
